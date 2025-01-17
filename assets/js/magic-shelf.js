42e8d9507ff5ad250899e2bd16c066a2a40cf2b4
Carlos Vidal <97411831+agui2393@users.noreply.github.com>
2025-01-17 16:58:05 +0000

    Adding function to magic-shelf Js  file that allows to change chapter when click on button. Adding CSS styling

--- assets/js/magic-shelf.js
+++ assets/js/magic-shelf.js
@@ -2,3 +2,22 @@ function toggleMenu() {
     const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active');
 }
+
+
+let currentChapter = 1;
+
+function nextPage() {
+    if (currentChapter === 1) {
+        document.getElementById("chapterOne").style.display = "none";
+        document.getElementById("chapterTwo").style.display = "flex";
+        currentChapter = 2;
+    } else if (currentChapter === 2) {
+        document.getElementById("chapterTwo").style.display = "none";
+        document.getElementById("chapterThree").style.display = "flex";
+        currentChapter = 3;
+    } else if (currentChapter === 3) {
+        document.getElementById("chapterThree").style.display = "none";
+        document.getElementById("chapterOne").style.display = "flex";
+        currentChapter = 1;
+    }
+}
